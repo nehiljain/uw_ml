d028d257efad8e3fe7f23a2fb06350fe741a135a
jain_nehil <jain.nehil@gmail.com>
2018-03-14 00:51:52 -0400

    working on assignment

--- classification/week_5_2.py
+++ classification/week_5_2.py
@@ -103,3 +103,51 @@ def intermediate_node_weighted_mistakes(labels_in_node, data_weights):
         return (weighted_mistakes_all_positive, +1)
     
 
+
+# In[ ]:
+
+
+# If the data is identical in each feature, this function should return None
+
+def best_splitting_feature(data, features, target, data_weights):
+    
+    # These variables will keep track of the best feature and the corresponding error
+    best_feature = None
+    best_error = float('+inf') 
+    num_points = float(len(data))
+
+    # Loop through each feature to consider splitting on that feature
+    for feature in features:
+        
+        # The left split will have all data points where the feature value is 0
+        # The right split will have all data points where the feature value is 1
+        left_split = data[data[feature] == 0]
+        right_split = data[data[feature] == 1]
+        
+        # Apply the same filtering to data_weights to create left_data_weights, right_data_weights
+        ## YOUR CODE HERE
+        left_data_weights = data_weights[data[feature] == 0]
+        right_data_weights = data_weights[data[feature] == 1]
+                    
+        # DIFFERENT HERE
+        # Calculate the weight of mistakes for left and right sides
+        ## YOUR CODE HERE
+        left_weighted_mistakes, left_class = intermediate_node_weighted_mistakes(left_split[target],
+                                                                                left_data_weights)
+        right_weighted_mistakes, right_class = intermediate_node_weighted_mistakes(right_split[target],
+                                                                                right_data_weights)
+        
+        # DIFFERENT HERE
+        # Compute weighted error by computing
+        #  ( [weight of mistakes (left)] + [weight of mistakes (right)] ) / [total weight of all data points]
+        ## YOUR CODE HERE
+        error = ...
+        
+        # If this is the best error we have found so far, store the feature and the error
+        if error < best_error:
+            best_feature = feature
+            best_error = error
+    
+    # Return the best feature we found
+    return best_feature
+
